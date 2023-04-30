ea2499983660a039717578b87a09323a2e5ae53c
bulbajs <93475048+bulbajs@users.noreply.github.com>
2023-04-30 22:05:39 +0300

    d4 module
    + Валидация через clean()

--- simpleapp/forms.py
+++ simpleapp/forms.py
@@ -1,5 +1,9 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import Product
+
+
 
 class ProductForm(forms.ModelForm):
     class Meta:
@@ -11,3 +15,26 @@ class ProductForm(forms.ModelForm):
             'price',
             'quantity'
         ]
+
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     description = cleaned_data.get('description')
+    #     if description is len(description) < 20:
+    #         raise ValidationError({
+    #             'description': 'Ошибка, братан'
+    #         })
+    #
+    #     return cleaned_data
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+        description = cleaned_data.get("description")
+
+        if description is not None and len(description) < 20:
+            raise ValidationError({
+                'description': "Описание не может быть менее 20 символов."
+            })
+
+        return cleaned_data
+
